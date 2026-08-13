f6924e2915c165a5bdc5c4ea61ee70a6b56397e8
Zoltan Kovacs <157127975+ZoltanSmith@users.noreply.github.com>
2026-08-13 18:41:52 +0200

    Add project files.

---
+++ ASPRendezveny/Models/Rendezveny.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASPRendezveny.Models;
+
+//[Table("rendezveny")]
+public partial class Rendezveny
+{
+    //[Column("id")]
+    public uint Id { get; set; }
+
+    [Required(AllowEmptyStrings = false, ErrorMessage = "")]
+    public uint ResztvevokSzama { get; set; }
+
+    public string? Elnevezes { get; set; }
+
+    public DateTime Idopont { get; set; }
+
+    public bool Torolt { get; set; }
+}
---
+++ ASPRendezveny/Models/RendezvenyContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ASPRendezveny.Models;
+
+public partial class RendezvenyContext : DbContext
+{
+    public RendezvenyContext()
+    {
+    }
+
+    public RendezvenyContext(DbContextOptions<RendezvenyContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Rendezveny> Rendezvenies { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<Rendezveny>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("rendezveny");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Elnevezes)
+                .HasMaxLength(100)
+                .HasColumnName("elnevezes");
+            entity.Property(e => e.Idopont)
+                .HasColumnType("datetime")
+                .HasColumnName("idopont");
+            entity.Property(e => e.ResztvevokSzama).HasColumnName("resztvevok_szama");
+            entity.Property(e => e.Torolt).HasColumnName("torolt");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
